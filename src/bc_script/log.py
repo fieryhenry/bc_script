fd8fd4be1736463b6bb349b74aec9f27c5d2a1eb
fieryhenry <github@henrythomas.me.uk>
2024-06-02 14:51:58 +0100

    colorama for logging

--- src/bc_script/log.py
+++ src/bc_script/log.py
@@ -1,3 +1,6 @@
+import colorama
+
+
 class Log:
     def __init__(
         self,
@@ -13,23 +16,35 @@ class Log:
         self.show_errors = show_errors
         self.show_info = show_info
 
+        colorama.init()
+
     def add_warning(self, warning: str):
         if self.show_warnings:
-            print(f"WARNING: {warning}")
+            print(f"{colorama.Fore.YELLOW}WARNING: {warning}{colorama.Style.RESET_ALL}")
         self.warnings.append(warning)
 
     def add_error(self, error: str):
         if self.show_errors:
-            print(f"ERROR: {error}")
+            print(f"{colorama.Fore.RED}ERROR: {error}{colorama.Style.RESET_ALL}")
         self.errors.append(error)
 
     def add_info(self, info: str):
         if self.show_info:
-            print(f"INFO: {info}")
+            print(
+                f"{colorama.Fore.LIGHTBLACK_EX}INFO: {info}{colorama.Style.RESET_ALL}"
+            )
         self.info.append(info)
 
     def print(self):
         if self.show_errors:
-            print(f"Finished with {len(self.errors)} errors")
+            color = colorama.Fore.RED if len(self.errors) > 0 else colorama.Fore.GREEN
+            print(
+                f"{color}Finished with {len(self.errors)} errors{colorama.Style.RESET_ALL}"
+            )
         if self.show_warnings:
-            print(f"Finished with {len(self.warnings)} warnings")
+            color = (
+                colorama.Fore.YELLOW if len(self.warnings) > 0 else colorama.Fore.GREEN
+            )
+            print(
+                f"{color}Finished with {len(self.warnings)} warnings{colorama.Style.RESET_ALL}"
+            )
